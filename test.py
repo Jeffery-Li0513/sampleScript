0bfe01ce5e370f20551ebc520001d3fdf6d6aff1
1072910262@qq.com <1072910262@qq.com>
2023-01-03 18:30:41 +0800

    20230103

--- test.py
+++ test.py
@@ -88,6 +88,53 @@ class neuralNetwork:
 
         return final_outputs
 
+def polt_confusion_matrix(cm, savename, title='Confusion Matrix'):
+    '''
+    定义画混淆矩阵的函数
+    :param cm: 计算得到的混淆矩阵
+    :param savename: 每个类的标签
+    :param title: 标题
+    :return:
+    '''
+    plt.figure(figsize=(12, 8), dpi=100)
+    np.set_printoptions(precision=2)
+
+    # 在混淆矩阵种中每格的概率值
+    ind_array = np.array(len(classes))
+    x, y = np.meshgrid(ind_array, ind_array)
+    for x_val, y_val in zip(x.flatten(), y.flatten()):
+        c = cm[y_val][x_val]
+        if c > 0.001:
+            plt.text(x_val, y_val, "%d" % (c,), color='red', fontsize=15, va='center', ha='center')
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
+    plt.title(title)
+    plt.colorbar()
+    xlocations = np.array(range(len(classes)))
+    plt.xticks(xlocations, classes, rotation=90)
+    plt.yticks(xlocations, classes)
+    plt.ylabel('Actual label')
+    plt.xlabel('Predict label')
+
+    # offset of tick
+    tick_marks = np.array(range(len(classes))) + 0.5
+    plt.gca().set_xticks(tick_marks, minor=True)
+    plt.gca().set_yticks(tick_marks, minor=True)
+    plt.gca().xaxis.set_ticks_position('none')
+    plt.gca().yaxis.set_ticks_position('none')
+    plt.grid(True, which='minor', linestyle='-')
+    plt.gcf().subplots_adjust(bottom=0.15)
+
+    # show confusion matrix
+    plt.savefig(savename, format='png')
+    plt.show()
+
+def load_data(data_folder):
+    '''
+    载入数据
+    :param data_folder:数据来源
+    :return: 训练集和测试集的数据及标签
+    '''
+
 
 
 
